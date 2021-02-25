6c493526db5f7b46792ede1e41b2e956c966152d
DavidReu <dadoo97441@gmail.com>
2021-02-25 14:01:39 +0400

    test path log

--- index.php
+++ index.php
@@ -16,12 +16,11 @@ use Monolog\Handler\FirePHPHandler;
 // Create the logger
 $logger = new Logger('my_logger');
 // Now add some handlers
-$logger->pushHandler(new StreamHandler(__DIR__ . '/my_app.log', Logger::DEBUG));
+$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
 $logger->pushHandler(new FirePHPHandler());
 
 // You can now use your logger
-/* $logger->info('My logger is now ready');
-dd($logger); */
+$logger->info('My logger is now ready');
 
 
 //---------- Use ----------
