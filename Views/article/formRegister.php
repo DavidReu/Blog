14366737f2144031f221e979e71d303f2c2d3d24
DavidReu <dadoo97441@gmail.com>
2021-02-25 15:10:26 +0400

    début résolution w3c sur le formulaire d'insription

--- Views/article/formRegister.php
+++ Views/article/formRegister.php
@@ -6,7 +6,7 @@
             <div class="formRegister px-5 py-5">
                 <h5 class="mt-3">Inscrivez-vous</h5>
                 <p class="mt-2 text-muted">Cela vous donne la possibilité de laisser des commentaires sur les articles</p>
-                <div class="form-input"> <i class="fa fa-envelope"></i> <input type="mail" class="form-control" placeholder="Votre mail" name="mail" required pattern="<?php echo $pattern ?>"> </div>
+                <div class="form-input"> <i class="fa fa-envelope"></i> <input type="email" class="form-control" placeholder="Votre mail" name="mail" required pattern='<?php echo $pattern ?>'> </div>
                 <div class="form-input"> <i class="fa fa-user"></i> <input type="text" class="form-control" placeholder="Votre nom" name="nom" required pattern="^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"> </div>
                 <div class="form-input"> <i class="fa fa-user"></i> <input type="text" class="form-control" placeholder="Votre prenom" name="prenom" required pattern="^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"> </div>
                 <div class="form-input"> <i class="fa fa-lock"></i> <input type="password" class="form-control" placeholder="Mot de passe" name="mdp" id="password" onkeyup='check()' required pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"> </div>
@@ -21,4 +21,4 @@
         </form>
     </div>
 </div>
-<script type="text/javascript" src="/public/js/app.js"></script>
+<script src="/public/js/app.js"></script>
