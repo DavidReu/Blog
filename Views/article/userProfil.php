b67737e9600bec73ac5f4ce8a2a409dfef5b46d6
DavidReu <dadoo97441@gmail.com>
2021-03-08 10:05:26 +0400

    modification de la page profil

--- Views/article/userProfil.php
+++ Views/article/userProfil.php
@@ -4,53 +4,52 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 $session = new Session();
 ?>
-<div class="container my-2">
+<div class="container-fluid">
     <div class="row">
-        <h4>Mon profil</h4>
-    </div>
-    <div class="row">
-        <section class="w-50">
-            <?php ?>
-            <div class="d-flex justify-content-around">
-                <span>Nom : <?php echo $profil->{'nom'} ?> </span>
-                <span>Prénom : <?php echo $profil->{'prenom'} ?></span>
-            </div>
-            <div class="d-flex justify-content-around">
-                <span>Mail : <?php echo $profil->{'mail'} ?></span>
-            </div>
-            <div>
-                <a href="/profil/modifier">Modifier mes imformations</a>
-            </div>
-        </section>
-    </div>
-    <div class="container my-2">
-        <div>
-            <h4>Mes commentaires</h4>
-        </div>
-        <div>
+        <div class="col col-3 bg-secondary p-3 text-center" style="height: 100vh;">
             <section>
-                <ul>
-                    <?php foreach ($comments as $key => $value) : ?>
-                        <li><?php echo $value['content'] ?> <a href="/article?id=<?php echo $value['articleId'] ?>">Voir l'article</a></li>
-                    <?php endforeach ?>
-                </ul>
+                <h4>Mon profil</h4>
+                <?php ?>
+                <div class="d-flex flex-column">
+                    <span>Nom : <?php echo $profil->{'nom'} ?> </span>
+                    <span>Prénom : <?php echo $profil->{'prenom'} ?></span>
+                    <span>Mail : <?php echo $profil->{'mail'} ?></span>
+                </div>
+                <div>
+                    <a href="/profil/modifier">Modifier mes imformations</a>
+                </div>
             </section>
         </div>
-    </div>
-    <?php if ($session->get('editor') == true) : ?>
-        <div class="container my-2">
+        <div class="my-3 col offset-1 col-6">
             <div>
-                <h4>Mes articles</h4>
+                <h4>Mes commentaires</h4>
             </div>
             <div>
                 <section>
                     <ul>
-                        <?php foreach ($articles as $key => $value) : ?>
-                            <li><?php echo $value['titre'] ?> <a href="/article?id=<?php echo $value['id'] ?>">Voir l'article</a></li>
+                        <?php foreach ($comments as $key => $value) : ?>
+                            <li><?php echo $value['content'] ?> <a href="/article?id=<?php echo $value['articleId'] ?>">Voir l'article</a></li>
                         <?php endforeach ?>
                     </ul>
                 </section>
             </div>
         </div>
-    <?php endif ?>
+        <?php if ($session->get('editor') == true) : ?>
+            <div class="container my-2">
+                <div>
+                    <h4>Mes articles</h4>
+                </div>
+                <div>
+                    <section>
+                        <ul>
+                            <?php foreach ($articles as $key => $value) : ?>
+                                <li><?php echo $value['titre'] ?> <a href="/article?id=<?php echo $value['id'] ?>">Voir l'article</a></li>
+                            <?php endforeach ?>
+                        </ul>
+                    </section>
+                </div>
+            </div>
+        <?php endif ?>
+    </div>
+
 </div>
