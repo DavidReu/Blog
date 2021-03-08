b871f5f307ab11d18a53920e21f6901c13db07f3
DavidReu <dadoo97441@gmail.com>
2021-03-08 10:44:20 +0400

    modification message de connexion retour à l'origine

--- Views/auth/messagelog.php
+++ Views/auth/messagelog.php
@@ -1,11 +1,17 @@
-<div d="messageLog" class="position-fixed bottom-0 end-0 p-3" style="z-index: 5">
+<div id="messageLog" class="alert alert-info text-center">
+    <span>
+        <?php echo $message  ?>
+    </span>
+</div>
+
+<!-- <div id="messageLog" class="position-fixed bottom-0 end-0 p-3" style="z-index: 5">
     <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
             <strong class="me-auto">Bootstrap</strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body">
-            <?php echo $message  ?>
+            <?php// echo $message  ?>
         </div>
     </div>
-</div>
+</div> -->
